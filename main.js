ba93ce96b454c41db6c1a8548016b16f9bb3492e
Noah Semus <33107099+noahsemus@users.noreply.github.com>
2021-12-16 17:29:26 -0500

    push to fart

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+try {
+  require('electron-reloader')(module)
+} catch (_) {}
+
 const express = require('./spotify-push-to-like')
 //import fetch from 'node-fetch';
-const { app, BrowserWindow, globalShortcut, net } = require('electron')
+const {
+  app,
+  BrowserWindow,
+  globalShortcut,
+  net
+} = require('electron')
 const Store = require('electron-store')
 //const net = electron.remote.net;
 const path = require('path')
@@ -8,77 +17,77 @@ const store = new Store()
 
 const EXP_URL = "http://localhost:8888"
 
-function createWindow () {
-    const win = new BrowserWindow({
-      width: 800,
-      height: 600,
-      autoHideMenuBar: true,
-      transparent: true, 
-      frame: false,
-      webPreferences: {
-        preload: path.join(__dirname + "/scripts", 'preload.js')
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    autoHideMenuBar: true,
+    transparent: true,
+    frame: false,
+    webPreferences: {
+      preload: path.join(__dirname + "/scripts", 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+async function fetchExpressUrl(endpoint) {
+  var requestApi = {
+    method: 'GET',
+    url: EXP_URL + endpoint
+  }
+
+  var resp
+  const request = new net.ClientRequest(requestApi);
+
+  request.on('response', d => {
+    console.log("logging from electron")
+    console.log(`STATUS: ${d.statusCode}`)
+    d.on('data', chunk => {
+      if (chunk.toString().startsWith('{')) {
+        let json = JSON.parse(chunk.toString())
+        console.log("JSON:")
+        json.tracks.forEach(track => console.log(track))
+        console.log(`track count: ${json.tracks.length}`)
       }
     })
-  
-    win.loadFile('index.html')
+  });
+  request.end();
 }
 
-async function fetchExpressUrl(endpoint){
-    var requestApi = {
-        method: 'GET',
-        url: EXP_URL + endpoint
-    }
-
-    var resp
-    const request = new net.ClientRequest(requestApi);
-
-    request.on('response', d => { 
-      console.log("logging from electron")
-      console.log(`STATUS: ${d.statusCode}`)
-      d.on('data', chunk => {
-        if(chunk.toString().startsWith('{')){
-          let json = JSON.parse(chunk.toString())
-          console.log("JSON:")
-          json.tracks.forEach(track => console.log(track))
-          console.log(`track count: ${json.tracks.length}`)
-        }
-      })
-    });
-    request.end();
-}
-
-function authWindow () {
-    const auth = new BrowserWindow({
-        width: 900,
-        height: 900
-    })
-    auth.loadURL("http://localhost:8888/login")
+function authWindow() {
+  const auth = new BrowserWindow({
+    width: 900,
+    height: 900
+  })
+  auth.loadURL("http://localhost:8888/login")
 }
 
 app.whenReady().then(() => {
-    globalShortcut.register('CommandOrControl+L', () => {
-        fetchExpressUrl("/like-current-track")
+  globalShortcut.register('CommandOrControl+L', () => {
+    fetchExpressUrl("/like-current-track")
+  })
+  globalShortcut.register('CommandOrControl+B', () => {
+    //let tracks = await fetchExpressUrl("/likedTracks")
+    //let tracks
+    fetchExpressUrl("/liked-tracks").then(() => {
+      //store.set('test','test')
+      //console.log(`logging from fetch: ${store.get('test')}`)
     })
-    globalShortcut.register('CommandOrControl+B', () => {
-      //let tracks = await fetchExpressUrl("/likedTracks")
-      //let tracks
-      fetchExpressUrl("/liked-tracks").then(() => {
-        //store.set('test','test')
-        //console.log(`logging from fetch: ${store.get('test')}`)
-      })
-      //console.log(tracks)
-    })
+    //console.log(tracks)
+  })
 }).then(createWindow).then(() => {
-    require('electron').shell.openExternal(EXP_URL + '/login');
+  require('electron').shell.openExternal(EXP_URL + '/login');
 })
-  
 
-  app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
-  })
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
